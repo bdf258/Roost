3401fc197260e622727c1159b564df26448addaf
Ben Dunn <67321059+bdf258@users.noreply.github.com>
2023-04-06 15:59:43 +0000

    Just the findahome webapp bit

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,6 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import './index.css'
 import App from './App'
-import Finance from './pages/finance.jsx'
-import MyHome from './pages/my-home-maintenance.jsx'
-import Settings from './pages/settings'
 import FindAHome from './pages/find-a-home'
 import HomesAvailable from './pages/homes-available'
 
@@ -15,10 +12,7 @@ ReactDOM.render(
   <React.StrictMode>
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Finance />} />
-        <Route path="/my-home" element={<MyHome />} />
-        <Route path="/settings" element={<Settings />} />
-        <Route path="/find-a-home" element={<FindAHome />} />
+        <Route path="/" element={<FindAHome />} />
         <Route path="/homes-available" element={<HomesAvailable />} />
       </Routes>
     </BrowserRouter>
